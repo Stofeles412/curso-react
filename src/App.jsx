ea31e0e3494d828683170d3e649eb311aced1eef
Stofeles412 <fernandostofeles@gmail.com>
2025-11-22 15:29:05 -0300

    .

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+function App(){
+  let message = "hello mundo";
+  return(
+  <div>
+<h1>{message}</h1>
+<button onClick={() > {menssage = "fui clicado"} } > message("ola fui mudado")mudar msg</button>
+  </div>
+
+  );
+
+}
+export default App;
